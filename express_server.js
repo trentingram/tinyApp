9d0b7f3cb559f3c984066affb0c845d3aee56bbd
trentingram <tingram@homeaway.com>
2019-01-15 19:30:58 +0000

    adds new route urls/:id for displaying long and short url

--- express_server.js
+++ express_server.js
@@ -14,6 +14,14 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+app.get("/urls/:id", (req, res) => {
+    var paramId = req.params.id;
+    
+    let templateVars = { shortURL: paramId,
+                        longURL: urlDatabase[paramId] };
+    res.render("urls_show", templateVars);
+  });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });

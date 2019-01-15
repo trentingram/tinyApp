968f5fedddb89c34ba9e524800abee828975ddca
trentingram <tingram@homeaway.com>
2019-01-15 18:35:53 +0000

    initial commit, adds all progress so far.

---
+++ express_server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var PORT = 8080;
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
